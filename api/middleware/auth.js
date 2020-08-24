255d1d69d870081cec31ee65d8cb6676be3b5743
Victor Oliveira <jvoliveiralive@gmail.com>
2020-08-24 17:03:32 -0300

    Testing secret to TTS -> LUA
    Don't know if it's possible to use JWT auth with LUA and TTS integration.

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -3,20 +3,25 @@ const jwt = require("jsonwebtoken");
 
 function auth(req, res, next) {
   const token = req.header("x-auth-token");
+  const secret = req.header("secret-key");
 
   // Check for token
   if (!token) {
     res.status(401).json({ error: { msg: "No token, authorization denied" } });
   }
-  try {
-    // Verify token
-    const decoded = jwt.verify(token, keys.JWT);
-
-    // Add user from payload;
-    req.user = decoded;
+  if (secret === keys.JWT) {
     next();
-  } catch (err) {
-    res.status(400).json({ error: { msg: "Token is not valid" } });
+  } else {
+    try {
+      // Verify token
+      const decoded = jwt.verify(token, keys.JWT);
+
+      // Add user from payload;
+      req.user = decoded;
+      next();
+    } catch (err) {
+      res.status(400).json({ error: { msg: "Token is not valid" } });
+    }
   }
 }
 
